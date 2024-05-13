78bf0390b552003a0a783e57877545bfd7aac864
Vantuong <vantuong.147@gmail.com>
2024-05-13 07:58:17 +0700

    config admin home address left button

--- admin.php
+++ admin.php
@@ -40,7 +40,7 @@ checkLogin();
 <body>
 <!-- Top navbar -->
 <nav class="navbar navbar-expand navbar-dark bg-dark">
-    <a class="navbar-brand" href="../home.php">Trang chủ</a>
+    <a class="navbar-brand" href="">Trang chủ</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
